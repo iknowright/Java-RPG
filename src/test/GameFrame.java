2006ce2dc8296427fc323519c60652520b3f9de9
wang0630 <j2081499@gmail.com>
2018-05-18 17:15:08 +0800

    Project prototype

---
+++ src/test/GameFrame.java
@@ -0,0 +1,11 @@
+// Main function
+package test;
+import util.*;
+import javax.swing.JFrame;
+
+public class GameFrame {
+	public static void main(String args[]) {
+		JFrame gameFrame  = new JFrame();
+		FullSize.fullScreen(gameFrame);
+	}
+}
--- src/util/FullSize.java
+++ src/util/FullSize.java
@@ -1,12 +1,13 @@
+// Make frame full screen and set it visible
 package util;
-import javax.swing.*;
 
-public class FullSize{
+import javax.swing.JFrame;
 
-  public static void fullSize(JFrame frame){
-    frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
-    frame.setUndecorated(true);
-    frame.setVisible(true);
-  }
-
+public class FullSize {
+	
+	public static void fullScreen(JFrame frame) {
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+		frame.setUndecorated(true);
+		frame.setVisible(true);
+	}
 }
