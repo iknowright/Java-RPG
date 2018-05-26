47fb49d19f8b713f8aa5a81d388b258ca3a768da
WeichiChen1210 <iphone31302@gmail.com>
2018-05-26 17:14:54 +0800

    first video

--- src/game/Game.java
+++ src/game/Game.java
@@ -17,16 +17,16 @@ public class Game extends Canvas implements Runnable {
 
 	public static final int WIDTH = 160;
 	public static final int HEIGHT = WIDTH / 12 * 9;
-	public static final int SCALE = 3;
+	public static final int SCALE = 8;
 	public static final String NAME = "Game";
 
 	private JFrame frame;
 
 	public boolean running = false;
-	public int tickCount=0;
+	public int tickCount = 0;
 	
-	private BufferedImage image=new BufferedImage(WIDTH,HEIGHT,BufferedImage.TYPE_INT_RGB);
-	//private int pixels =((DataBufferInt)image.getRaster().getDataBuffer()).getDataType();	
+	private BufferedImage image = new BufferedImage(WIDTH,HEIGHT, BufferedImage.TYPE_INT_RGB);
+	private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
 
 	public Game() {
 		setMinimumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
@@ -78,13 +78,13 @@ public class Game extends Canvas implements Runnable {
 				delta -= 1;
 				shouldRender = true;
 			}
+/*			
 			try {
 				Thread.sleep(2);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+*/			
 			if (shouldRender) {
 				frames++;
 				render();
@@ -92,7 +92,7 @@ public class Game extends Canvas implements Runnable {
 			}
 			if (System.currentTimeMillis() - lastTimer >= 1000) {
 				lastTimer += 1000;
-				System.out.println(ticks + "ticks," + frames+"frames");
+				System.out.println(ticks + " ticks, " + frames+" frames");
 				frames = 0;
 				ticks = 0;
 			}
@@ -102,25 +102,25 @@ public class Game extends Canvas implements Runnable {
 
 	public void tick() {
 		tickCount++;
+		for(int i = 0; i < pixels.length; i++) {
+			pixels[i] = i + tickCount;
+		}
 	}
 
 	public void render() {
-		BufferStrategy bs=getBufferStrategy();
-		if(bs==null) {
+		BufferStrategy bs = getBufferStrategy();
+		if(bs == null) {
 			createBufferStrategy(3);
 			return;
 		}
 		
 		Graphics g=bs.getDrawGraphics();
-		g.setColor(Color.BLACK);
-		g.fillRect(0,0,getWidth(),getHeight());
-		
+		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
 		g.dispose();
 		bs.show();
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		new Game().start();
 	}
 
